9656126c3c81720b67f38fd67b10f010b430ed90
Bao Nguyen <nguyen@simi>
2024-10-12 16:27:07 +0200

    .

--- imagecrop.py
+++ imagecrop.py
@@ -106,8 +106,11 @@ def croppedFace(image_id):
     #profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
+    if len(faces)==0:
+         profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+         faces = profile_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     # Assuming you take the first detected face (you can find the one closest to the head joint)
+
     for (x, y, w, h) in faces:
         print('hi')
         face_crop = img[y:y+h, x:x+w]
