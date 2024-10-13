d1d9742899f226b68b4c7db891aca9848e918454
Bao Nguyen <nguyen@simi>
2024-10-13 20:58:46 +0200

    .

--- imagecrop.py
+++ imagecrop.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
 
     p = pickle.load(open('/home/nguyenbt/nobackup/data/2019_Mhse_Height_Data/train.pickle', 'rb'), encoding='latin1')
     print(str(p["image_id"][0]))
-    for i, img_id in enumerate(p["image_id"]):
+    for i, img_id in enumerate(p["image_id"][38782:], start=38782):
         index = i
         print(i)
         croppedBodywholeimgheight(i)
