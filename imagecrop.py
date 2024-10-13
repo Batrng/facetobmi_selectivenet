1ab5aa7c6aebd2a4dd260f0033f8ec4161ac27aa
Bao Nguyen <nguyen@simi>
2024-10-13 22:28:29 +0200

    .

--- imagecrop.py
+++ imagecrop.py
@@ -143,6 +143,7 @@ if __name__ == "__main__":
     p = pickle.load(open('/home/nguyenbt/nobackup/data/2019_Mhse_Height_Data/train.pickle', 'rb'), encoding='latin1')
     print(str(p["image_id"][0]))
     
+    #for i, img_id in enumerate(p["image_id"][38781:], start=38781):
     for i, img_id in enumerate(p["image_id"][38781:], start=38781):
         index = i
         print(i)
