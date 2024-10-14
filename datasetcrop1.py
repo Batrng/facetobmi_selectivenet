394f74080d30c60cf92dea972be4482df3973aa4
Bao Nguyen <nguyen@simi>
2024-10-14 10:41:23 +0200

    .

--- datasetcrop1.py
+++ datasetcrop1.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
     #croppedBody()
 
     p = pickle.load(open('/home/nguyenbt/nobackup/data/2019_Mhse_Height_Data/val.pickle', 'rb'), encoding='latin1')
-    print(str(p["image_id"][38782]))
+    #print(str(p["image_id"][0]))
     
     #for i, img_id in enumerate(p["image_id"][38781:], start=38781):
     for i, img_id in enumerate(p["image_id"]):
