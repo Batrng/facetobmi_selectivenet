024551d3f64f95add6b230ea08ff1f24d58a4ade
Bao Nguyen <nguyen@simi>
2024-10-20 14:43:16 +0200

    .

--- run.py
+++ run.py
@@ -26,6 +26,7 @@ def train(train_loader, features, model, loss_selective, optimizer):
         # Compute prediction error
         pred, pred_select, pred_aux  = model(X_fullbody, X_face)
         #y = y.unsqueeze(1).float()
+        y = y.float()
         selective_loss = loss_selective(pred, pred_select, y, pred_aux, train=True)
         selective_loss *= alpha
 
@@ -73,6 +74,7 @@ def validate(val_loader, model, loss_selective):
             # Compute prediction error
             pred, pred_select, pred_aux  = model(X_fullbody, X_face)
             #y = y.squeeze(1).float()
+            y = y.float()
             selective_loss = loss_selective(pred, pred_select, y, pred_aux,val=True)
             selective_loss *= alpha
 
